c701750bb97fc91cf424a7e4290210c321a2c1fc
PPedroso <pedroso_pedro@hotmail.com>
2022-07-14 21:53:56 +0100

    force commit to publish

--- Website/Program.cs
+++ Website/Program.cs
@@ -16,7 +16,6 @@ namespace Website
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
             await builder.Build().RunAsync();
-            //
         }
     }
 }
